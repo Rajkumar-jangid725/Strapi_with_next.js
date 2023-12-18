5e13ac620fb9eded15cffa9567d073d479a02c97
Rajkumar Jangid <jsshyam1997@gmail.com>
2023-12-18 12:33:50 +0530

    demo

---
+++ frontend/src/app/[lang]/components/PageHeader.tsx
@@ -0,0 +1,13 @@
+interface PageHeaderProps {
+  heading: string,
+  text?: string,
+}
+
+export default function PageHeader({ heading, text } : PageHeaderProps) {
+  return (
+    <div className="my-16 w-full text-center">
+    { text && <span className="text-violet-400 font-bold">{text}</span> }
+    <h2 className="text-4xl font-bold font-heading hover:text-yellow-500">{heading}</h2>
+  </div>
+  );
+}
