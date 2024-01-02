5e92c8818badc5f3b848afac21d60479bb10ac83
Rajkumar Jangid <jsshyam1997@gmail.com>
2024-01-02 12:17:25 +0530

    Sitemap configuration

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -17,24 +17,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  sitemap: {
+    enabled: true,
+    config: {
+      cron: '0 0 0 * * *',
+      limit: 45000,
+      xsl: true,
+      autoGenerate: false,
+      caching: true,
+      allowedFields: ['id', 'uid'],
+      excludedTypes: [],
+    },
+  },
 });
-
-// module.exports = ({ env }) => ({
-//   seo: {
-//     enabled: true,
-//   },
-//   comments: {
-//     enabled: true,
-//     config: {
-//       badWords: false,
-//       moderatorRoles: ["Authenticated"],
-//       approvalFlow: ["api::article.article"],
-//       entryLabel: {
-//         "*": ["Title", "title", "Name", "name", "Subject", "subject", "Website", "website", "Email", "email"],
-//         "api::article.article": ["*"],
-//       }, 
-//       reportReasons: {
-//       },
-//     },
-//   },
-// });
